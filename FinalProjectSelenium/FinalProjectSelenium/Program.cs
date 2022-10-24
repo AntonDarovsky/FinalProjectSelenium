fab6016b9e8ee04fe6f900e4edde0051f5dced5f
Anton Darovsky <antondarovski97@gmail.com>
2022-10-24 21:32:41 +0300

    master

---
+++ FinalProjectSelenium/FinalProjectSelenium/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalProjectSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
